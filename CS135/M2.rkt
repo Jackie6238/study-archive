0160333d80ec10ebc74547fc439c33a463e82679
Jackie6238 <78058592+Jackie6238@users.noreply.github.com>
2022-12-22 20:11:34 -0500

    Create M2.rkt

---
+++ CS135/M2.rkt
@@ -0,0 +1,31 @@
+;; Module2 - Functions
+;; Exercises
+
+;; ex. 1 omitted
+
+;; ex. 2
+(+ 2 3)
+(* 2 3)
+(- 44 2)
+(+ (* 3 4) 2)
+(/ (+ 2 4) (- 5 1))
+(* 3 (+ 1 (+ 6/2 5)))
+
+;; ex. 3 omitted
+;; ex. 4 omitted
+;; ex. 5 omitted
+
+;; ex. 6
+(define (add-twice a b)
+  (+ a (* 2 b)))
+
+;; ex. 7 omitted
+;; ex. 8 omitted
+;; ex. 9 omitted
+;; ex. 10 omitted
+
+;; ex. 11
+(define (g x y)
+  (+ (* x (sqrt x)) (sqr y)))
+
+;; ex. 12 omitted
